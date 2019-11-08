e2408a299fad21071936df500c05901c03a26e35
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-11-08 14:07:09 -0500

    Add project files.

---
+++ Hello Wear/Hello Wear/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Views;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Support.Wearable.Activity;
+using Java.Interop;
+using Android.Views.Animations;
+
+namespace Hello_Wear
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : WearableActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.click_button);
+            TextView text = FindViewById<TextView>(Resource.Id.result);
+
+            button.Click += delegate
+            {
+                text.Text = $"{count++} clicks!";
+            };
+
+            SetAmbientEnabled();
+        }
+    }
+}
+
+
